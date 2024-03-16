2cd5043a84325b08c006546e5cb5426f8d0c56af
Chau Nguyen <chau.nguyen1506@hcmut.edu.vn>
2024-03-16 23:11:18 +0700

    Fix tree entity migration

--- back-end/migrations/1710079363962-TreeEntity.ts
+++ back-end/migrations/1710079363962-TreeEntity.ts
@@ -5,8 +5,8 @@ export class TreeEntity1710079363962 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE \`note_closure\` (\`id_ancestor\` varchar(255) NOT NULL, \`id_descendant\` varchar(255) NOT NULL, INDEX \`IDX_c595484fba68bc88524030ce37\` (\`id_ancestor\`), INDEX \`IDX_924868788a237a99f9bb21e53b\` (\`id_descendant\`), PRIMARY KEY (\`id_ancestor\`, \`id_descendant\`)) ENGINE=InnoDB`);
-        await queryRunner.query(`ALTER TABLE \`note_closure\` ADD CONSTRAINT \`FK_c595484fba68bc88524030ce370\` FOREIGN KEY (\`id_ancestor\`) REFERENCES \`note\`(\`id\`) ON DELETE CASCADE ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE \`note_closure\` ADD CONSTRAINT \`FK_924868788a237a99f9bb21e53ba\` FOREIGN KEY (\`id_descendant\`) REFERENCES \`note\`(\`id\`) ON DELETE CASCADE ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE \`note_closure\` ADD CONSTRAINT \`FK_c595484fba68bc88524030ce370\` FOREIGN KEY (\`id_ancestor\`) REFERENCES \`note\`(\`id\`) ON DELETE CASCADE ON UPDATE CASCADE`);
+        await queryRunner.query(`ALTER TABLE \`note_closure\` ADD CONSTRAINT \`FK_924868788a237a99f9bb21e53ba\` FOREIGN KEY (\`id_descendant\`) REFERENCES \`note\`(\`id\`) ON DELETE CASCADE ON UPDATE CASCADE`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
