3e17e47ccafbd92017ae0a1e83851fd11e9c071e
Chau Nguyen <chau.nguyen1506@hcmut.edu.vn>
2024-03-26 10:57:52 +0700

    Update find note by collaborator and find note for anyone

--- back-end/src/note_collaborator/note_collaborator.controller.ts
+++ back-end/src/note_collaborator/note_collaborator.controller.ts
@@ -6,44 +6,45 @@ import {
   Patch,
   Param,
   Delete,
-} from "@nestjs/common";
-import { NoteCollaboratorService } from "./note_collaborator.service";
-import { CreateNoteCollaboratorDto } from "./dto/create-note_collaborator.dto";
-import { UpdateNoteCollaboratorDto } from "./dto/update-note_collaborator.dto";
-import { ApiTags } from "@nestjs/swagger";
+} from '@nestjs/common';
+import { NoteCollaboratorService } from './note_collaborator.service';
+import { CreateNoteCollaboratorDto } from './dto/create-note_collaborator.dto';
+import { UpdateNoteCollaboratorDto } from './dto/update-note_collaborator.dto';
+import { ApiTags } from '@nestjs/swagger';
+import { Public } from '../auth/auth.decorator';
 
-@ApiTags("note_collaborator")
-@Controller("api/note_collaborator/")
+@ApiTags('note_collaborator')
+@Controller('api/note_collaborator/')
 export class NoteCollaboratorController {
   constructor(
-    private readonly noteCollaboratorService: NoteCollaboratorService
+    private readonly noteCollaboratorService: NoteCollaboratorService,
   ) {}
 
-  @Post("add_note_collaborator")
+  @Post('add_note_collaborator')
   async createNoteCollaborator(
     @Body() createNoteCollaboratorDto: CreateNoteCollaboratorDto,
   ) {
     return await this.noteCollaboratorService.createNoteCollaborator(
-      createNoteCollaboratorDto
+      createNoteCollaboratorDto,
     );
   }
 
-  @Get("collaborators/:note_id")
-  async findCollaboratorsOfNote(@Param("note_id") note_id: string) {
+  @Get('collaborators/:note_id')
+  async findCollaboratorsOfNote(@Param('note_id') note_id: string) {
     return await this.noteCollaboratorService.findCollaboratorsOfNote(note_id);
   }
 
-  @Get(":note_id")
-  async findOneNoteWithEmail(
-    @Param("note_id") note_id: string,
+  @Get(':note_id')
+  async findOneNoteByCollaborator(
+    @Param('note_id') note_id: string,
     @Body()
     req: {
       email: string;
-    }
+    },
   ) {
-    return await this.noteCollaboratorService.findOneNoteWithEmail(
+    return await this.noteCollaboratorService.findOneNoteByCollaborator(
       note_id,
-      req
+      req,
     );
   }
 
--- back-end/src/note_collaborator/note_collaborator.service.ts
+++ back-end/src/note_collaborator/note_collaborator.service.ts
@@ -1,10 +1,10 @@
-import { Get, Injectable } from "@nestjs/common";
-import { CreateNoteCollaboratorDto } from "./dto/create-note_collaborator.dto";
-import { UpdateNoteCollaboratorDto } from "./dto/update-note_collaborator.dto";
-import { NoteCollaborator } from "./entities/note_collaborator.entity";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Equal, Repository } from "typeorm";
-import { Note } from "../note/entities/note.entity";
+import { Get, Injectable, NotAcceptableException } from '@nestjs/common';
+import { CreateNoteCollaboratorDto } from './dto/create-note_collaborator.dto';
+import { UpdateNoteCollaboratorDto } from './dto/update-note_collaborator.dto';
+import { NoteCollaborator } from './entities/note_collaborator.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Equal, Repository } from 'typeorm';
+import { Note } from '../note/entities/note.entity';
 
 @Injectable()
 export class NoteCollaboratorService {
@@ -12,10 +12,10 @@ export class NoteCollaboratorService {
     @InjectRepository(NoteCollaborator)
     private readonly noteCollaboratorRepository: Repository<NoteCollaborator>,
     @InjectRepository(Note)
-    private readonly noteRepository: Repository<Note>
+    private readonly noteRepository: Repository<Note>,
   ) {}
   async createNoteCollaborator(
-    createNoteCollaboratorDto: CreateNoteCollaboratorDto
+    createNoteCollaboratorDto: CreateNoteCollaboratorDto,
   ) {
     const newNoteCollaborator = this.noteCollaboratorRepository.create(
       createNoteCollaboratorDto,
@@ -52,23 +52,19 @@ export class NoteCollaboratorService {
     return { emails: retEmails, ...is_anyone };
   }
 
-  async findOneNoteWithEmail(note_id, req) {
-    return await this.noteCollaboratorRepository.find({
-      where: [
-        {
-          note_id: note_id,
-          note: {
-            is_anyone: true,
-          },
-        },
-        {
-          note_id: note_id,
-          email: req.email,
-        },
-      ],
+  async findOneNoteByCollaborator(note_id, req) {
+    const note = await this.noteCollaboratorRepository.findOne({
+      where: {
+        note_id: note_id,
+        email: req.email,
+      },
       relations: {
         note: true,
       },
     });
+    if (!note) {
+      throw new NotAcceptableException('Email is not invited');
+    }
+    return note;
   }
 }
